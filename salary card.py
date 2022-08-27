fc12f8e7832931dd881549c7d276eba3cc934df6
anadpashok <110198480+anadpashok@users.noreply.github.com>
2022-08-27 17:39:29 +0530

    Update salary card.py

--- salary card.py
+++ salary card.py
@@ -17,7 +17,7 @@ total_cost_to_company=base_salary+annual_short_term_bonus+other_allowance
 df = pd.DataFrame([[base_salary,base_salary//12],[HRA,HRA//12],[Special_Allowance,Special_Allowance//12],[employer_pf,employer_pf//12],[employer_ESI,employer_ESI//12],[base_salary,base_salary//12],[annual_short_term_bonus,0],[other_allowance,other_allowance/12],[total_cost_to_company,(total_cost_to_company//12)+(other_allowance//12)]],
                   index=['Basic Salary', 'HRA', 'Special Allowance','Employer PF','Employer ESI','Base Salary','Annual Short-Term Bonus','Other Allowance(internet)','Total Cost To Company'], columns=['Annual', 'Monthly'])
 print(df)
-
+df.to_csv("salary.csv")
 
 
 
